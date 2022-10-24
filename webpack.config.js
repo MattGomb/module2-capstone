0782799d00d48065da3f479560ce3327627913b7
Matyas Gombos <gombosmm@gmail.com>
2022-10-24 18:25:23 -0500

    Install linters

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devServer: {
-    static:'./dist',
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
