7cab73ee948ded588fe704bda861aa80a7333457
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-10-25 18:56:01 -0500

    change for loop

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,40 @@
+/* eslint-disable*/
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const mode = 'development';
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
-  module: {
+    module:   {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
+      }, 
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: 'asset',
       },
     ],
+  },
+  mode,
+  devServer: {
+    static: './dist',
+    historyApiFallback: true
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
