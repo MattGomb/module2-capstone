3261fcf90f61856a5c9863ae23b4a78ebe1ecc43
Oklukeok <lucasabermudez@gmail.com>
2022-10-25 13:58:50 -0600

    linters corrections

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import './style.css';
-import './modules/mainOutput.js'
-
+import './modules/mainOutput.js';
--- src/modules/mainOutput.js
+++ src/modules/mainOutput.js
@@ -1,70 +1,64 @@
 const codex = document.querySelector('#main-container');
 
 const retrieve = (item) => {
-	const card = document.createElement('div'); // Card Div
-	card.setAttribute('class', 'PokeCard');
-	codex.appendChild(card);
+  const card = document.createElement('div'); // Card Div
+  card.setAttribute('class', 'PokeCard');
+  codex.appendChild(card);
 
-	const pokeName = document.createElement('h3'); // Card Tittle
-	pokeName.classList.add('name');
-	pokeName.innerText = item.name;
-	card.appendChild(pokeName);
+  const pokeName = document.createElement('h3'); // Card Tittle
+  pokeName.classList.add('name');
+  pokeName.innerText = item.name;
+  card.appendChild(pokeName);
 
-	const picDiv = document.createElement('div');
-	picDiv.setAttribute('class', 'picDiv');
-	card.appendChild(picDiv);
+  const picDiv = document.createElement('div');
+  picDiv.setAttribute('class', 'picDiv');
+  card.appendChild(picDiv);
 
-	const picture = document.createElement('img');
-	picture.setAttribute('src', item.sprites.front_default);
-	picDiv.appendChild(picture);
+  const picture = document.createElement('img');
+  picture.setAttribute('src', item.sprites.front_default);
+  picDiv.appendChild(picture);
 
-	const intro = document.createElement('div');
-	intro.classList.add('intro')
-	card.appendChild(intro);
+  const intro = document.createElement('div');
+  intro.classList.add('intro');
+  card.appendChild(intro);
 
-    const type = document.createElement('p');
-    type.innerHTML = "Type: " + item.types[0].type.name;
-    intro.appendChild(type);
+  const type = document.createElement('p');
+  type.innerHTML = `Type: ${item.types[0].type.name}`;
+  intro.appendChild(type);
 
-    const height = document.createElement('p');
-    height.innerHTML = "Height: " + item.height + " '";
-    intro.appendChild(height);
+  const height = document.createElement('p');
+  height.innerHTML = `Height: ${item.height} '`;
+  intro.appendChild(height);
 
-    const weight = document.createElement('p');
-    weight.innerHTML = "weight: " + item.weight + " lbs";
-    intro.appendChild(weight);
+  const weight = document.createElement('p');
+  weight.innerHTML = `weight: ${item.weight} lbs`;
+  intro.appendChild(weight);
 
-
-
-
-
-
-	const buttonsDiv = document.createElement('div');
-	buttonsDiv.classList.add('buttons');
-	card.appendChild(buttonsDiv);
-	const commentBtn = document.createElement('button');
-	commentBtn.classList.add('comment');
-	commentBtn.innerText = 'Comments';
-	const reserveBtn = document.createElement('button');
-	reserveBtn.innerText = 'Reserve';
-	reserveBtn.classList.add('reserve');
-	buttonsDiv.appendChild(commentBtn);
-	buttonsDiv.appendChild(reserveBtn);
+  const buttonsDiv = document.createElement('div');
+  buttonsDiv.classList.add('buttons');
+  card.appendChild(buttonsDiv);
+  const commentBtn = document.createElement('button');
+  commentBtn.classList.add('comment');
+  commentBtn.innerText = 'Comments';
+  const reserveBtn = document.createElement('button');
+  reserveBtn.innerText = 'Reserve';
+  reserveBtn.classList.add('reserve');
+  buttonsDiv.appendChild(commentBtn);
+  buttonsDiv.appendChild(reserveBtn);
 };
 
-
-for(let i = 1; i<152;i++){
-	const link = 'https://pokeapi.co/api/v2/pokemon/';
-	const finalLink = link + i
+for (let i = 1; i < 152; i + 1) {
+  const link = 'https://pokeapi.co/api/v2/pokemon/';
+  const finalLink = link + i;
   const myFunction = async () => {
-		await fetch(finalLink)
-			.then((response) => response.json())
-			.then((json) => retrieve(json))
-			.catch((err) => console.log(err));
-	}
-	myFunction()
+    await fetch(finalLink)
+      .then((response) => response.json())
+      .then((json) => retrieve(json))
+      .catch((err) => console.log(err));
+  };
+  myFunction();
 }
-    
+
 /*
     const myFunction = async () => {
         await fetch('https://pokeapi.co/api/v2/pokemon/1/')
