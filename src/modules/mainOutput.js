23cf0d930bc1f4a44c2c91ddac3bec746a0292ee
Matyas Gombos <gombosmm@gmail.com>
2022-10-25 18:51:57 -0500

    Fix bug in loop that broke the whole computer

--- src/modules/mainOutput.js
+++ src/modules/mainOutput.js
@@ -47,7 +47,7 @@ const retrieve = (item) => {
   buttonsDiv.appendChild(reserveBtn);
 };
 
-for (let i = 1; i < 152; i + 1) {
+for (let i = 1; i < 152; i += 1) {
   const link = 'https://pokeapi.co/api/v2/pokemon/';
   const finalLink = link + i;
   const myFunction = async () => {
