6facfd8aed8d043ab2e9da6fcfe536a73ceb3c38
Matyas Gombos <gombosmm@gmail.com>
2022-10-26 18:04:40 -0500

    Complementary adjustments

---
+++ src/modules/commentAPI.js
@@ -0,0 +1,12 @@
+import createPopup from './popupCD.js'
+
+
+/* fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/q43Np4AB1ka0fqpIWSXs/comments')
+
+
+
+{
+  "item_id": "item1",
+  "username": "Jane",
+  "comment": "Hello"
+} */
