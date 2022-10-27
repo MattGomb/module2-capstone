65749792fdb99e932b2151309db61f4e87bb7857
Matyas Gombos <gombosmm@gmail.com>
2022-10-27 17:26:41 -0500

    Add counter function to comments (not working atm)

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import './style.css';
 import './modules/mainOutput.js';
 import './modules/reservation_popup.js';
 import './modules/popupCD.js';
+import './modules/commentCounter.js'
---
+++ src/modules/commentCounter.js
@@ -0,0 +1,9 @@
+/* const commentCounter = (classOfCount, append) => {
+  const counter = classOfCount.length;
+  const counterText = document.createElement('p');
+  counterText.setAttribute('id', 'counter');
+  counterText.textContent = `Total comments: ${counter}`;
+  append.appendChild(counterText);
+};
+
+export default commentCounter(); */
