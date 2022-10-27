e3d28d000182763d21e69841578beb936db3a58e
Matyas Gombos <gombosmm@gmail.com>
2022-10-27 14:27:41 -0500

    Change order of module imports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable linebreak-style */
 import './style.css';
 import './modules/mainOutput.js';
-import './modules/popupCD.js';
 import './modules/reservation_popup.js';
+import './modules/popupCD.js';
