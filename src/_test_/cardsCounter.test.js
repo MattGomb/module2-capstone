dbfe3f9000b6c6beb86eca6159edd4277f723900
Oklukeok <lucasabermudez@gmail.com>
2022-10-28 15:19:27 -0600

    Eslint corrections

--- src/_test_/cardsCounter.test.js
+++ src/_test_/cardsCounter.test.js
@@ -5,10 +5,8 @@
 
 import cardsNumber from '../modules/cardsCounter';
 
-
 test('counting cards', () => {
-  document.body.innerHTML =
-   '<h1 id="title"></h1>'
+  document.body.innerHTML = '<h1 id="title"></h1>'
     + '  <div class="PokeCard"></div>'
     + '  <div class="PokeCard"></div>'
     + '  <div class="PokeCard"></div>';
@@ -19,11 +17,8 @@ test('counting cards', () => {
   expect(cardsResult.textContent).toBe('PokeCodex (3)');
 });
 
-
 test('counting cards should be 0', () => {
-  document.body.innerHTML =
-   '<h1 id="title"></h1>'
-    
+  document.body.innerHTML = '<h1 id="title"></h1>';
 
   const cardsResult = document.getElementById('title');
 
